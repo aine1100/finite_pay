c7ad487d38790e24dfbace13df440179d0fd576d
aine1100 <149615514+aine1100@users.noreply.github.com>
2025-02-10 17:54:57 +0200

    remaining reponsiveness

--- src/app/constants/icon.ts
+++
@@ -1,9 +0,0 @@
-import appleStore from "@/app/assets/images/apple.png"
-import playStore from "@/app/assets/images/playstore.png"
-
-const logos=[
-    appleStore,
-    playStore
-]
-
-export default logos;
